b3f6641e9b954a73d08fea5e490d5c3ab812dcd4
Aerospace91 <beenters@gmail.com>
2025-08-15 09:43:46 -0700

    More unit tests

--- src/textnode.py
+++ src/textnode.py
@@ -1,5 +1,6 @@
 from enum import Enum
 from typing import Optional
+from htmlnode import LeafNode
 
 class TextType(Enum):
     """
@@ -70,4 +71,19 @@ class TextNode:
         Returns:
             str: The string representation.
         """
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"    
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+    
+def text_node_to_html_node(text_node):
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
