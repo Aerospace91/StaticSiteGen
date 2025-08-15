46dcd7c8953f767361444588631ab30ac67e19c6
Aerospace91 <beenters@gmail.com>
2025-08-15 08:50:56 -0700

    Added more functions and more unittests

---
+++ src/htmlnode.py
@@ -0,0 +1,46 @@
+from typing import List, Dict, Any, Optional
+
+class HTMLNode:
+    def __init__(self,
+                 tag = None,
+                 value = None,
+                 children = None,
+                 props = None
+            ):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+        
+    def to_html(self):
+        raise NotImplementedError("to_html method not implemented")
+    
+    def props_to_html(self):
+        if self.props is None:
+            return ""
+        props_html = ""
+        for prop in self.props:
+            props_html += f' {prop}="{self.props[prop]}"'
+        return props_html
+    
+    def __repr__(self):
+        return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props = None):
+        super().__init__(tag, value, None, props)
+        
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value")
+        elif self.tag is None:
+            return self.value
+        else:
+            if self.props is None:
+                return f'<{self.tag}>{self.value}</{self.tag}>'
+            else:
+                result = f'<{self.tag}'
+                for prop in self.props:
+                    result += f' {prop}="{self.props[prop]}"'
+                result += f'>{self.value}</{self.tag}>'
+                return result
